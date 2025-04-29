90b68d8af304a5e95e9bda859a2cb8fa0a77692b
Arushi9740 <arushimuskan004@gmail.com>
2025-04-29 18:43:18 +0530

    adding file

--- test01.py
+++ test01.py
@@ -1,6 +1,6 @@
-import sepp01 as sepp01
+import sepp01 as s
 
-assert sepp01.create_user("abc","abc@gmail.com","9036076747","Ma63621111ma") == "Account created successfully"
-assert sepp01.create_user("def","def@gmail.com","635412","abcdefgh586453") == "Invalid phone number"
-assert sepp01.verify_otp("123456","123456") == "otp verified"
-assert sepp01.verify_captcha("AB75m") == "Captcha verified"
+assert s.create_user("abc","abc@gmail.com","9036076747","Ma63621111ma") == "Account created successfully"
+assert s.create_user("def","def@gmail.com","635412","abcdefgh586453") == "Invalid phone number"
+assert s.verify_otp("123456","123456") == "otp verified"
+assert s.verify_captcha("AB75m") == "Captcha verified"
